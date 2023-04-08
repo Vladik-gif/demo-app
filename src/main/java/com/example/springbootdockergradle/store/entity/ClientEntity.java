0a4b669a87d2999625d985b45512f44fb0d03d7b
Vladik <v4054741@gmail.com>
2023-04-08 23:30:55 +0300

    create class Entity

---
+++ src/main/java/com/example/springbootdockergradle/store/entity/ClientEntity.java
@@ -0,0 +1,40 @@
+package com.example.springbootdockergradle.store.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "client")
+public class ClientEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String citi;
+
+    public ClientEntity(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getCiti() {
+        return citi;
+    }
+
+    public void setCiti(String citi) {
+        this.citi = citi;
+    }
+}
