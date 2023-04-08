4e6f28b3d7d2719f279808dea6f35386bc5b9fdf
Vladik <v4054741@gmail.com>
2023-04-08 23:34:23 +0300

    Add method: clients and homw

---
+++ src/main/java/com/example/springbootdockergradle/controllers/HomeController.java
@@ -0,0 +1,28 @@
+package com.example.springbootdockergradle.controllers;
+
+import com.example.springbootdockergradle.store.entity.ClientEntity;
+import com.example.springbootdockergradle.store.repository.ClientRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class HomeController {
+
+    private final ClientRepository clientRepository;
+
+    public HomeController(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    @GetMapping("/clients")
+    public List<ClientEntity> clients(){
+        return clientRepository.findAll();
+    }
+
+    @GetMapping("/")
+    public String home(){
+        return "Hello!";
+    }
+}
