fecfe64c5e9270695d424e96881cfe3bd0f83313
Vladik <v4054741@gmail.com>
2023-05-03 22:22:48 +0300

    CRUD client restful api

---
+++ src/main/java/com/example/springbootdockergradle/api/controllers/ClientController.java
@@ -0,0 +1,40 @@
+package com.example.springbootdockergradle.api.controllers;
+
+import com.example.springbootdockergradle.api.service.ClientServer;
+import com.example.springbootdockergradle.store.entity.ClientEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+public class ClientController {
+
+    private final ClientServer clientServer;
+    private static final String CREATE = "/create";
+    private static final String GETS = "/clients";
+    private static final String GETID = "get/{id}";
+    private static final String DELETEID = "/delete/{id}";
+
+    public ClientController(ClientServer clientServer) {
+        this.clientServer = clientServer;
+    }
+
+    @PostMapping(CREATE)
+    public ClientEntity create(@RequestBody ClientEntity client){
+        return clientServer.create(client);
+    }
+
+    @GetMapping(GETS)
+    public List<ClientEntity> clients(){
+        return clientServer.clients();
+    }
+
+    @GetMapping(GETID)
+    public ClientEntity getId(@PathVariable Long id){
+        return clientServer.getId(id);
+    }
+
+    @DeleteMapping(DELETEID)
+    public void deleteId(@PathVariable Long id){
+        clientServer.deleteId(id);
+    }
+}
