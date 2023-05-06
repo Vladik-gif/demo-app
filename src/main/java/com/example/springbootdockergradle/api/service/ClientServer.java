60d6e26682ada294a6f3f0d48e2368f689017666
Vladik <v4054741@gmail.com>
2023-05-06 18:09:12 +0300

    minor fixes

--- src/main/java/com/example/springbootdockergradle/api/service/ClientServer.java
+++ src/main/java/com/example/springbootdockergradle/api/service/ClientServer.java
@@ -6,12 +6,10 @@ import jakarta.transaction.Transactional;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-@EnableCaching
 public class ClientServer {
 
     private final ClientRepository clientRepository;
@@ -19,25 +17,25 @@ public class ClientServer {
     public ClientServer(ClientRepository clientRepository) {
         this.clientRepository = clientRepository;
     }
-
-    @Transactional
     @CachePut(value = "create")
     public ClientEntity create(ClientEntity client){
         return clientRepository.save(client);
     }
 
-    @Cacheable(value = "getId", key = "#id")
+    @Cacheable(
+            value = "getId",
+            key = "#id",
+            unless = "#result.id < 12000"
+    )
     public ClientEntity getId(Long id){
         return clientRepository.getReferenceById(id);
     }
 
-    @Transactional
-    @Cacheable(value = "clients")
+    @CachePut(value = "clients")
     public List<ClientEntity> clients(){
         return clientRepository.findAll();
     }
 
-    @Transactional
     @CacheEvict(value = "deleteId", key = "#id")
     public void deleteId(Long id){
         clientRepository.deleteById(id);
