3305feb0103c4dfa2fbe073363a2797118540bfe
Vladik <v4054741@gmail.com>
2023-05-09 20:01:58 +0300

    minor fixes

--- src/main/java/com/example/springbootdockergradle/api/service/ClientServer.java
+++ src/main/java/com/example/springbootdockergradle/api/service/ClientServer.java
@@ -2,13 +2,16 @@ package com.example.springbootdockergradle.api.service;
 
 import com.example.springbootdockergradle.store.entity.ClientEntity;
 import com.example.springbootdockergradle.store.repository.ClientRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
+@EnableCaching
 public class ClientServer {
 
     private final ClientRepository clientRepository;
@@ -16,25 +19,26 @@ public class ClientServer {
     public ClientServer(ClientRepository clientRepository) {
         this.clientRepository = clientRepository;
     }
+
+    @Transactional
     @CachePut(value = "create")
     public ClientEntity create(ClientEntity client){
         return clientRepository.save(client);
     }
 
-    @Cacheable(
-            value = "getId",
-            key = "#id",
-            unless = "#result.id < 12000"
-    )
+    @Transactional
+    @Cacheable(value = "getId", key = "#id")
     public ClientEntity getId(Long id){
         return clientRepository.getReferenceById(id);
     }
 
+    @Transactional
     @CachePut(value = "clients")
     public List<ClientEntity> clients(){
         return clientRepository.findAll();
     }
 
+    @Transactional
     @CacheEvict(value = "deleteId", key = "#id")
     public void deleteId(Long id){
         clientRepository.deleteById(id);
--- src/main/java/com/example/springbootdockergradle/store/entity/ClientEntity.java
+++ src/main/java/com/example/springbootdockergradle/store/entity/ClientEntity.java
@@ -1,29 +1,17 @@
 package com.example.springbootdockergradle.store.entity;
 
 import jakarta.persistence.*;
-
-import java.io.Serial;
 import java.io.Serializable;
 
 @Entity
 @Table(name = "clients")
 public class ClientEntity implements Serializable {
-    @Serial
-    private static final long serialVersionUID = 7156526077883281623L;
-
     @Id
-    @SequenceGenerator(name = "SEQ_GEN", sequenceName = "SEQ_USER", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "SEQ_GEN")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String email;
     public ClientEntity() {}
-
-    public ClientEntity(String username,
-                        String email) {
-        this.username = username;
-        this.email = email;
-    }
 
     public Long getId() {
         return id;
@@ -44,7 +32,6 @@ public class ClientEntity implements Serializable {
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
